aa9bfecee451621080e05f9fb76214d5a5e13a41
Setoju <setoju48@gmail.com>
2025-04-16 00:43:03 +0300

    Add player movement logic and sprite animation.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int currentSpriteIndex = 0;
+
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float jumpForce = 5f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * jumpForce;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * jumpForce;
+            }
+        }
+
+
+        // because gravity is m^2 we multiply it by deltaTime twice
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        currentSpriteIndex++;
+        if (currentSpriteIndex >= sprites.Length)
+        {
+            currentSpriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[currentSpriteIndex];
+    }
+}
